4fe1c0ca4dbaed45352e0efd3527c5a54f2b8f48
marcelopy <emramirez@hotmail.com>
2018-11-04 21:18:24 +0100

    pills done, almost final style

--- bandProject.js
+++ bandProject.js
@@ -1,1 +1,22 @@
-console.log(`let s Gooooo it`);
+let blogTabs = document.querySelectorAll('.pills ul li');
+let blogText = document.querySelectorAll('.blogContainer div');
+
+
+let toggleText = function(arrayOfText, typeOfClass) {
+  for (let j = 0; j < arrayOfText.length; j++) {
+    if (arrayOfText[j].classList.contains(typeOfClass)) {
+      arrayOfText[j].classList.remove(typeOfClass);
+      break;
+    }
+  }
+}
+
+for (let i = 0; i < blogTabs.length; i++) {
+  blogTabs[i].addEventListener('click', function(ev) {
+    toggleText(blogText, 'active');
+    toggleText(blogTabs, 'selected');
+
+    blogText[i].classList.add('active');
+    blogTabs[i].classList.add('selected');
+  })
+}
