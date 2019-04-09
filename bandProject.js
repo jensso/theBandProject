9cbafc6915614f175e7e913153b55be278a716fb
jens soltwedel <jens.soltwedel@hotmail.com>
2019-04-09 04:15:30 +0200

    style details added

--- bandProject.js
+++ bandProject.js
@@ -1,7 +1,24 @@
+let scrollHeight = document.querySelectorAll('.scrollHeight');
+let navTabs = document.querySelectorAll('nav li');
+let yAxisOfSections = [];
+for (t=0; t<scrollHeight.length; t++){
+  yAxisOfSections.push((scrollHeight[t].offsetTop)-150);
+};
+console.log(yAxisOfSections);
+window.addEventListener(`scroll`, function(){
+
+    for(j=0; j<scrollHeight.length; j++){
+      if(window.pageYOffset>=yAxisOfSections[j] && window.pageYOffset<yAxisOfSections[j+1]){
+      navTabs[j+1].classList.add(`scrollActive`);
+    }
+      else {navTabs[j+1].classList.remove(`scrollActive`);
+    }
+  }
+})
+
+
 let blogTabs = document.querySelectorAll('.pills ul li');
 let blogText = document.querySelectorAll('.blogContainer div');
-
-
 let toggleText = function(arrayOfText, typeOfClass) {
   for (let j = 0; j < arrayOfText.length; j++) {
     if (arrayOfText[j].classList.contains(typeOfClass)) {
@@ -10,7 +27,6 @@ let toggleText = function(arrayOfText, typeOfClass) {
     }
   }
 }
-
 for (let i = 0; i < blogTabs.length; i++) {
   blogTabs[i].addEventListener('click', function(ev) {
     toggleText(blogText, 'active');
@@ -25,7 +41,6 @@ let arrowLeft = document.querySelector(`.carousel > span:nth-of-type(1)`);
 let arrowRight = document.querySelector(`.carousel > span:nth-of-type(2)`);
 let pictures = document.querySelectorAll(`.slider`);
 let count = 0;
-
 arrowLeft.addEventListener(`click`, function(ev) {
   count++;
 
@@ -63,27 +78,24 @@ arrowRight.addEventListener(`click`, function(ev) {
     pictures[`${count}`].style.height = `45vw`;
 })
 
-
 let member = document.querySelectorAll(`.bandImages > img`);
 let info = document.querySelectorAll(`.memberInfo`);
-
 for (let i = 0; i < member.length; i++) {
-member[i].addEventListener(`click`, function showInfo(ev) {
+member[i].addEventListener(`click`, function(ev) {
 
   for (let j = 0; j < info.length; j++) {
-    if (info[j].style.display === `flex`) {
+    if (info[j].style.display === `block`) {
       info[j].style.display = `none`;
-      break;
+      // break;
     }
-    info[j].style.display = `flex`;
   }
+  info[i].style.display = `block`;
+
 })
 }
-
 let closeButton = document.querySelector(`.ticketPopUp > p > span`);
 let orderBox = document.querySelector(`.ticketPopUp`);
 let cancelButton = document.querySelector(`.cancel`);
-
 closeButton.addEventListener(`click`, function(ev) {
   orderBox.style.display = `none`;
 })
@@ -91,9 +103,7 @@ cancelButton.addEventListener(`click`, function(ev) {
   orderBox.style.display = `none`;
 })
 
-
 let buyButton = document.querySelectorAll(`.location > button`);
-
 for (let i = 0; i < buyButton.length; i++) {
   buyButton[i].addEventListener(`click`, function(ev) {
     orderBox.style.display = `flex`;
